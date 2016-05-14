adb7a10fa7235ed5d89a7e5357bb374de45f3422
Dave Yarwood <dave.yarwood@gmail.com>
2016-05-14 00:10:12 -0400

    tinkering

--- src/ews/db.cljs
+++ src/ews/db.cljs
@@ -12,19 +12,6 @@
   [db f]
   (.serialize db #(f db)))
 
-; example from node-sqlite3 readme
-; (this is a literal js translation, super ugly cljs code)
-(defn test-sqlite3 [& [db-file]]
-  (let [test-db (fn [db]
-                  (.run db "CREATE TABLE lorem (info TEXT)")
-                  (let [stmt (.prepare db "INSERT INTO lorem VALUES (?)")]
-                    (dotimes [n 10]
-                      (.run stmt (str "Ipsum " n)))
-                    (.finalize stmt))
-                  (.each db "SELECT rowid AS id, info FROM lorem"
-                    (fn [e row]
-                      (js/console.log (str (.-id row) ": " (.-info row)))))) ]
-    (doto (new sqlite3.Database (or db-file ":memory:"))
-      (serialize test-db)
-      (.close))))
-
+(defn migrate
+  "TODO (using db-migrate node package)"
+  [])
