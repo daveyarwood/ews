4848f5ca3481caab4ed84ef70aeccf8cbfe1e010
Dave Yarwood <dave.yarwood@gmail.com>
2016-08-07 14:04:34 -0400

    macros are dope as fuck

--- src/db/migrations.rs
+++ src/db/migrations.rs
@@ -2,77 +2,66 @@ use rusqlite;
 use schemamama::Migrator;
 use schemamama_rusqlite::{SqliteAdapter, SqliteMigration, SqliteMigrationError};
 
-struct CreateUsers;
-migration!(CreateUsers, 1470499978, "create users table");
+macro_rules! ews_migration {
+    ( $version:expr, $migration:ident, $desc:expr, $up:expr, $down:expr) => {
+        struct $migration;
+        migration!($migration, $version, $desc);
 
-struct CreateCases;
-migration!(CreateCases, 1470499979, "create cases table");
+        impl SqliteMigration for $migration {
+            fn up(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
+                conn.execute($up, &[]).map(|_| ())
+            }
 
-struct CreateItems;
-migration!(CreateItems, 1470499980, "create items table");
-
-struct CreateNotes;
-migration!(CreateNotes, 1470499981, "create notes table");
-
-impl SqliteMigration for CreateUsers {
-    fn up(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
-        conn.execute("CREATE TABLE ews_user(\
-                        id INTEGER PRIMARY KEY, \
-                        name TEXT NOT NULL\
-                      );", &[]).map(|_| ())
-    }
-
-    fn down(&self, connection: &rusqlite::Connection) -> rusqlite::Result<()> {
-        connection.execute("DROP TABLE ews_user;", &[]).map(|_| ())
+            fn down(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
+                conn.execute($down, &[]).map(|_| ())
+            }
+        }
     }
 }
 
-impl SqliteMigration for CreateCases {
-    fn up(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
-        conn.execute("CREATE TABLE ews_case(\
-                        id INTEGER PRIMARY KEY, \
-                        title TEXT NOT NULL, \
-                        userid INTEGER NOT NULL, \
-                        openeddate INTEGER NOT NULL, \
-                        closeddate INTEGER, \
-                        FOREIGN KEY(userid) REFERENCES ews_user(id)\
-                      );", &[]).map(|_| ())
-    }
+ews_migration!(1470499978, CreateUsers, "create users table",
+               "CREATE TABLE ews_user(\
+                  id INTEGER PRIMARY KEY, \
+                  name TEXT NOT NULL\
+                );",
+                "DROP TABLE ews_user;");
 
-    fn down(&self, connection: &rusqlite::Connection) -> rusqlite::Result<()> {
-        connection.execute("DROP TABLE ews_case;", &[]).map(|_| ())
-    }
-}
+ews_migration!(1470499979, CreateCases, "create cases table",
+               "CREATE TABLE ews_case(\
+                  id INTEGER PRIMARY KEY, \
+                  title TEXT NOT NULL, \
+                  userid INTEGER NOT NULL, \
+                  openeddate INTEGER NOT NULL, \
+                  closeddate INTEGER, \
+                  FOREIGN KEY(userid) REFERENCES ews_user(id)\
+                );",
+                "DROP TABLE ews_case;");
 
-impl SqliteMigration for CreateItems {
-    fn up(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
-        conn.execute("CREATE TABLE ews_item(\
-                        id INTEGER PRIMARY KEY, \
-                        title TEXT NOT NULL, \
-                        caseid INTEGER NOT NULL, \
-                        followupdate INTEGER, \
-                        FOREIGN KEY(caseid) REFERENCES ews_case(id)\
-                      );", &[]).map(|_| ())
-    }
+ews_migration!(1470499980, CreateItems, "create items table",
+               "CREATE TABLE ews_item(\
+                  id INTEGER PRIMARY KEY, \
+                  title TEXT NOT NULL, \
+                  caseid INTEGER NOT NULL, \
+                  followupdate INTEGER, \
+                  FOREIGN KEY(caseid) REFERENCES ews_case(id)\
+                );",
+                "DROP TABLE ews_item;");
 
-    fn down(&self, connection: &rusqlite::Connection) -> rusqlite::Result<()> {
-        connection.execute("DROP TABLE ews_item;", &[]).map(|_| ())
-    }
-}
+ews_migration!(1470499981, CreateNotes, "create notes table",
+               "CREATE TABLE ews_note(\
+                  id INTEGER PRIMARY KEY, \
+                  itemid INTEGER NOT NULL, \
+                  body TEXT, \
+                  modifieddate INTEGER NOT NULL, \
+                  FOREIGN KEY(itemid) REFERENCES ews_item(id)\
+                );",
+                "DROP TABLE ews_note;");
 
-impl SqliteMigration for CreateNotes {
-    fn up(&self, conn: &rusqlite::Connection) -> rusqlite::Result<()> {
-        conn.execute("CREATE TABLE ews_note(\
-                        id INTEGER PRIMARY KEY, \
-                        itemid INTEGER NOT NULL, \
-                        body TEXT, \
-                        modifieddate INTEGER NOT NULL, \
-                        FOREIGN KEY(itemid) REFERENCES ews_item(id)\
-                      );", &[]).map(|_| ())
-    }
-
-    fn down(&self, connection: &rusqlite::Connection) -> rusqlite::Result<()> {
-        connection.execute("DROP TABLE ews_note;", &[]).map(|_| ())
+macro_rules! register {
+    ( $migrator:expr, $( $migration:ident ),* ) => {
+        $(
+            $migrator.register(Box::new($migration));
+         )*
     }
 }
 
@@ -81,18 +70,7 @@ pub fn run(conn: &rusqlite::Connection) -> Result<(), SqliteMigrationError> {
     adapter.setup_schema();
 
     let mut migrator = Migrator::new(adapter);
-
-    let migrations: Vec<Box<SqliteMigration>> = vec![
-        Box::new(CreateUsers),
-        Box::new(CreateCases),
-        Box::new(CreateItems),
-        Box::new(CreateNotes),
-    ];
-
-    for migration in migrations {
-        migrator.register(migration);
-    }
-
-    // bogus number that will be higher than any epoch timestamp
+    register!(migrator, CreateUsers, CreateCases, CreateItems, CreateNotes);
+    // FIXME: bogus number that will be higher than any epoch timestamp
     migrator.up(1999999999)
 }
